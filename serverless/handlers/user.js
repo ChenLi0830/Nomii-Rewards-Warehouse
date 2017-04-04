e033416f6cf996ec50a2e421bed7142609ad6095
Chen Li <yichen.li0830@gmail.com>
2017-04-04 16:31:44 -0700

    Configuring staging DynamoDB users table to trigger lambda user method

---
+++ serverless/handlers/user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const main = (event, context, callback) => {
+  console.log("event", event);
+  console.log("event.Records", event.Records);
+  console.log("event.Records[0]", event.Records[0]);
+  console.log("event.Records[0].dynamodb", event.Records[0].dynamodb);
+  
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'User updates',
+      input: event,
+    }),
+  };
+  
+  callback(null, response);
+  
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports = main;
