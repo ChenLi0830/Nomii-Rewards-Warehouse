2c1e9613c7efade7cd7fc432b50d6a4aed31180d
Chen Li <yichen.li0830@gmail.com>
2017-04-04 22:44:02 -0700

    Hooking user task with User table, read old and new records

--- serverless/handlers/user.js
+++ serverless/handlers/user.js
@@ -6,6 +6,11 @@ const main = (event, context, callback) => {
   console.log("event.Records[0]", event.Records[0]);
   console.log("event.Records[0].dynamodb", event.Records[0].dynamodb);
   
+  const {OldImage, NewImage} = event.Records[0].dynamodb;
+  console.log("OldImage", OldImage);
+  console.log("NewImage", NewImage);
+  // event.Records[0].dynamodb.NewImage
+  
   const response = {
     statusCode: 200,
     body: JSON.stringify({
